63a72392c8f002e00729e42e6dad4c8810a58db8
Lsong <song940@gmail.com>
2024-12-03 15:50:59 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,51 @@
 #include <M5StickC.h>
 
-float gyroX, gyroY, gyroZ;
+float flowRate = 0.0;
+float totalLitres = 0.0;
+unsigned long oldTime = 0;
+const float calibrationFactor = 7.5;
+const int flowSensorPin = 26; // G26
 
-void setup()
-{
-  M5.begin();
-  M5.IMU.Init();
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setRotation(3);
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.println("Hello world!");
+volatile int pulseCount = 0;
+
+void IRAM_ATTR pulseCounter() {
+  pulseCount++;
 }
 
+void setup() {
+  M5.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(1);
+  
+  pinMode(flowSensorPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(flowSensorPin), pulseCounter, RISING);
+  
+  oldTime = millis();
+}
 
-void loop()
-{
-  M5.Lcd.setCursor(0, 10, 2);
-  M5.Rtc.GetBm8563Time();
-  M5.Lcd.printf("%02d:%02d:%02d\n", M5.Rtc.Hour, M5.Rtc.Minute, M5.Rtc.Second);
-  M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
-  M5.Lcd.printf("X:%7.2f\n", gyroX);
-  M5.Lcd.printf("Y:%7.2f\n", gyroY);
-  M5.Lcd.printf("Z:%7.2f\n", gyroZ);
-  delay(100);
+void loop() {
+  if((millis() - oldTime) >= 1000) {
+    detachInterrupt(digitalPinToInterrupt(flowSensorPin));
+    
+    float timeInterval = millis() - oldTime;
+    flowRate = ((1000.0 / timeInterval) * pulseCount) / calibrationFactor;
+    totalLitres += (pulseCount / calibrationFactor);
+    
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 10);
+    M5.Lcd.printf("Flow Rate: %.2f L/min", flowRate);
+    M5.Lcd.setCursor(0, 30);
+    M5.Lcd.printf("Total: %.2f L", totalLitres);
+    
+    pulseCount = 0;
+    oldTime = millis();
+    
+    attachInterrupt(digitalPinToInterrupt(flowSensorPin), pulseCounter, RISING);
+  }
+  
+  M5.update();
+  if(M5.BtnA.wasPressed()) {
+    totalLitres = 0;
+  }
 }
